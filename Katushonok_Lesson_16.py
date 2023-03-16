886bc69af24c6325f8156c62160c3531958a425f
Katushonok <kotusch1993@gmail.com>
2023-03-16 20:16:34 +0300

    ДЗ 16

--- Katushonok_Lesson_16.py
+++ Katushonok_Lesson_16.py
@@ -1,44 +1,60 @@
 class Calkulator:
 
-    def __init__(self, val_1=int(input('Введите первое число: ')), val_2=int(input('Введите второе число: ')), operat=input('Введите операцию для калькулятора: ')):
-        self.num_1 = val_1
-        self.num_2 = val_2
-        self.oper = operat
+    # def __init__(self, val_1=None, val_2=None, operat=None):
+    #     self.num_1 = val_1
+    #     self.num_2 = val_2
+    #     self.oper = operat
+    #
+    #     if self.oper == '+':
+    #         print(self.add())
+    #
+    #     elif self.oper == '-':
+    #         print(self.minus())
+    #
+    #     elif self.oper == '*':
+    #         print(self.umpn())
+    #
+    #     elif self.oper == '/':
+    #         if self.num_2 != 0:
+    #             print(self.delen())
+    #         else:
+    #             print('На ноль делить нельзя!')
 
-        if self.oper == '+':
-            print(self.add())
+    def add(self, num_1, num_2):
+        return num_1 + num_2
 
-        elif self.oper == '-':
-            print(self.minus())
+    def minus(self, num_1, num_2):
+        return num_1 - num_2
 
-        elif self.oper == '*':
-            print(self.umpn())
+    def umpn(self, num_1, num_2):
+        return num_1 * num_2
 
-        elif self.oper == '/':
-            if self.num_2 != 0:
-                print(self.delen())
-            else:
-                print('На ноль делить нельзя!')
-
-
-
-    def add(self):
-        return self.num_1 + self.num_2
-
-    def minus(self):
-        return self.num_1 - self.num_2
-
-    def umpn(self):
-        return self.num_1 * self.num_2
-
-    def delen(self):
-        return self.num_1 / self.num_2
+    def delen(self, num_1, num_2):
+        return num_1 / num_2
 
 cal1 = Calkulator()
 
-# while True:
-#     oper = input('Ведите операцию для калькулятора(+, - ,/, *, или N для завершения: )')
-#     if oper != 'N':
-#         Calkulator(oper)
-#     else: break
+while True:
+    oper = input('Ведите операцию для калькулятора(+, - ,/, *, или N для завершения: ')
+    if oper != 'N':
+        num_1 = float(input('Введите первое число: '))
+        num_2 = float(input('Введите второе число: '))
+        if oper == '+':
+            print(cal1.add(num_1, num_2))
 
+        if oper == '-':
+            print(cal1.minus(num_1, num_2))
+
+        if oper == '*':
+            print(cal1.umpn(num_1, num_2))
+
+        if oper == '/':
+            if num_2 != 0:
+                print(cal1.delen(num_1, num_2))
+            else:
+                print('На ноль делить нельзя!')
+
+    else: break
+
+print(cal1.minus(10, 5))
+print(cal1.add(10, 5))
