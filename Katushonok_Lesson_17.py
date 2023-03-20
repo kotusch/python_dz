a2334b020f92f1bb2f03feb56cc9b19ff640779b
Katushonok <kotusch1993@gmail.com>
2023-03-20 20:34:35 +0300

    ДЗ 17(ред)

--- Katushonok_Lesson_17.py
+++ Katushonok_Lesson_17.py
@@ -1,24 +1,32 @@
 #ДЗ на вторник:
 # В задаче про класс Human дописать 2 класса: класс House для создания дома, класс Buy_House для его покупки.
 # Для того, чтобы в классе Human свойство __house сделать True, нужно использовать наследование классов. Но каких? :)
+class House:
+    def __init__(self, area, number_floors, price):
+        self.area = area
+        self.number_floors = number_floors
+        self.price = price
+
+    def house_info(self):
+        print(f'Площадь дома: {self.area} кв.м, этажность дома: {self.number_floors} этажей, цена: {self.price} тыс.')
 
 
 
-
-class Buy_House:
+class Buy_House(House):
+    def __init__(self, area, number_floors, price):
+        super().__init__(area, number_floors, price)
 
     def buy_house(self):
         self.__house = True
 
 
 # Создайте класс Human.
-class Human(Buy_House):
+class Human:
 # Определите для него два статических поля: default_name и default_age.
     default_name = 'USER'
     default_age = None
 # Создайте метод __init__(), который помимо self принимает еще два параметра: name и age. Для этих параметров задайте значения по умолчанию, используя свойства default_name и default_age. В методе __init__() определите четыре свойства: Публичные - name и age. Приватные - money и house.
     def __init__(self, name = default_name, age = default_age):
-        # super().__init__()
         #Динамические поля
         #Публичные
         self.name = name
@@ -32,9 +40,11 @@ class Human(Buy_House):
         print(f'Имя: {self.name}, возраст: {self.age}, наличие дома: {self.__house}, денег: {self.__money}')
 
     def buy_house(self):
-        self.__house = True
-        self.__money -= 100
-
+        if h1.price < self.__money:
+            self.__house = True
+            self.__money -= h1.price
+        else:
+            print("Не хватает денег!")
 # Реализуйте справочный статический метод default_info(), который будет выводить статические поля default_name и default_age.
     @staticmethod
     def default_info():
@@ -48,21 +58,8 @@ class Human(Buy_House):
 
 
 
-class House:
-    def __init__(self, area, number_floors, price):
-        self.area = area
-        self.number_floors = number_floors
-        self.price = price
-
-    def house_info(self):
-        print(f'Площадь дома: {self.area} кв.м, этажность дома: {self.number_floors} этажей, цена: {self.price} тыс.')
-
-
-
-
 p1 = Human("Evgeniy", 29)
+h1 = House(100, 2, 10000)
+h1.house_info()
 p1.buy_house()
 p1.info()
-
-h1 = House(100, 2, 100)
-h1.house_info()
